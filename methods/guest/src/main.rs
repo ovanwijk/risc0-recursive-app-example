8a13faad58392fb96b0acd9b2a8a2d1ab57a1f7f
Olaf van Wijk <o_van_wijk@hotmail.com>
2024-09-17 07:52:27 +0200

    WIP

---
+++ methods/guest/src/main.rs
@@ -0,0 +1,16 @@
+use risc0_zkvm::{guest::env, serde};
+
+
+fn main() {
+   
+    let (image_id, counter): ([u32; 8], u64) = env::read();
+    
+    if counter == 0 {        
+        env::commit(&(image_id, 1u64));
+        return;
+    }    
+    env::verify(image_id, &serde::to_vec(&(image_id, counter)).unwrap()).unwrap();    
+    env::commit(&(image_id, (counter + 1) as u64));
+    println!("{:?}", env::cycle_count());
+    
+}
